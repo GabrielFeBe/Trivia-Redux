fb6fa91a5f25000a7f17f47b377098ae091be01f
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-03-20 14:55:15 -0300

    req 8

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -7,11 +7,41 @@ const zeroOFive = 0.5;
 export default class Question extends Component {
   state = {
     trigger: false,
+    seconds: 30,
+    arrayOfQuestions: [],
+    timeOut: false,
+  };
+
+  componentDidMount() {
+    const { question } = this.props;
+    const array = [...question.incorrect_answers, question.correct_answer].sort(
+      () => Math.random() - zeroOFive,
+    );
+    this.startTime();
+    this.setState({ arrayOfQuestions: array,
+    });
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    const TIME_LIMIT = 1;
+    if (prevState.seconds === TIME_LIMIT) {
+      clearInterval(this.intervalID);
+      this.setState({ timeOut: true });
+    }
+  }
+
+  startTime = () => {
+    const ONE_SECOND = 1000;
+    this.intervalID = setInterval(() => {
+      this.setState((prev) => ({
+        seconds: prev.seconds - 1,
+      }));
+    }, ONE_SECOND);
   };
 
   render() {
     const { question } = this.props;
-    const { trigger } = this.state;
+    const { trigger, arrayOfQuestions, seconds, timeOut } = this.state;
     return (
       <div>
         <p data-testid="question-category">{question.category}</p>
@@ -19,17 +49,18 @@ export default class Question extends Component {
         <div
           data-testid="answer-options"
         >
-          {[...question.incorrect_answers, question.correct_answer].sort(
-            () => Math.random() - zeroOFive,
-          ).map((answer, index) => {
+          <div>{seconds}</div>
+          {arrayOfQuestions.map((answer, index) => {
             if (answer === question.correct_answer) {
               return (
                 <button
                   key={ index }
+                  disabled={ timeOut }
                   data-testid="correct-answer"
                   className={ trigger && 'rigth ' }
                   // para Ligar as cores
                   onClick={ () => {
+                    clearInterval(this.intervalID);
                     this.setState({ trigger: true });
                   } }
                 >
@@ -42,9 +73,11 @@ export default class Question extends Component {
               <button
                 data-testid={ `wrong-answer-${index}` }
                 key={ index }
+                disabled={ timeOut }
                 className={ trigger && 'wrong' }
                 onClick={ () => {
                   // para ligar as cores
+                  clearInterval(this.intervalID);
                   this.setState({ trigger: true });
                 } }
               >
