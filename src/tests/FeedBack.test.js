98c64282e1d7c791cfe6730c99e03461b73c92d1
acmarchiori <ac_marchiori@yahoo.com.br>
2023-03-21 14:51:07 -0300

    Finalizado requisito 17

---
+++ src/tests/FeedBack.test.js
@@ -0,0 +1,70 @@
+import userEvent from '@testing-library/user-event';
+import React from "react";
+import { screen, waitFor } from "@testing-library/react";
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+import App from "../App";
+import token from './helpers/mockToken';
+describe('Testa a tela de FeedBack', () => {
+    test ('Verifica se as informações dessa tela estão apresentadas conforme solicitado ', () =>  {
+
+      const initialState = {
+        token: token,
+        player: {
+          name: 'Player Name',
+          gravatarEmail: 'player@email.com',
+          score: 0,
+          assertions: 0,
+        }
+      }
+      
+      //...
+        renderWithRouterAndRedux(<App />, initialState, "/feedback");
+
+        const img = screen.getByRole('img');
+        expect(img).toBeInTheDocument();
+
+        const playerName = screen.getByText(/player name/i);
+        expect(playerName).toBeInTheDocument();
+
+        const headerScore = screen.getByTestId('header-score');
+        expect(headerScore).toBeInTheDocument();
+
+        const feedbackText = screen.getByText(/could be better\.\.\./i);
+        expect(feedbackText).toBeInTheDocument();
+
+        const feedbackTotalScore = screen.getByTestId('feedback-total-score');
+        expect(feedbackTotalScore).toBeInTheDocument();
+
+        const feedbackTotalQuestion = screen.getByTestId('feedback-total-question');
+        expect(feedbackTotalQuestion).toBeInTheDocument();
+
+        const buttonPlayAgain = screen.getByRole('button', { name: /play again/i });
+        expect(buttonPlayAgain).toBeInTheDocument();
+
+        const buttonRanking = screen.getByRole('button', { name: /ranking/i });
+        expect(buttonRanking).toBeInTheDocument();
+    });
+    test('Verifica se as mensagens "Well Done!" aparecem na tela;', () => {
+      const initialState = {
+        token: token,
+        player: {
+          name: 'Player Name',
+          gravatarEmail: 'player@email.com',
+          score: 0,
+          assertions: 3,
+        }
+      }
+      
+      const { history } = renderWithRouterAndRedux(<App />, initialState, "/feedback");
+
+      const message2 = screen.getByTestId('feedback-text');
+      expect(message2).toBeInTheDocument();
+
+      const buttonPlayAgain = screen.getByRole('button', { name: /play again/i });
+      userEvent.click(buttonPlayAgain);
+
+      waitFor(() => { 
+        expect(history.location.pathname).toBe('/game');
+      });
+    });
+    });
---
+++ src/tests/helpers/mockQuestions.js
@@ -0,0 +1,65 @@
+const questions = {
+  "response_code": 0,
+  "results": [
+      {
+          "category": "Science: Computers",
+          "type": "boolean",
+          "difficulty": "medium",
+          "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;",
+          "correct_answer": "False",
+          "incorrect_answers": [
+              "True"
+          ]
+      },
+      {
+          "category": "Science & Nature",
+          "type": "multiple",
+          "difficulty": "hard",
+          "question": "What is the molecular formula of Ozone?",
+          "correct_answer": "O3",
+          "incorrect_answers": [
+              "C6H2O6",
+              "N2O",
+              "SO4"
+          ]
+      },
+      {
+          "category": "Entertainment: Video Games",
+          "type": "multiple",
+          "difficulty": "hard",
+          "question": "What was the world&#039;s first handheld game device?",
+          "correct_answer": "Mattel Auto Race",
+          "incorrect_answers": [
+              "Game Boy",
+              "Microvision",
+              "Game &amp; Watch"
+          ]
+      },
+      {
+          "category": "Entertainment: Video Games",
+          "type": "multiple",
+          "difficulty": "medium",
+          "question": "In the video game &quot;League of Legends&quot; which character is known as &quot;The Sinister Blade&quot;?",
+          "correct_answer": "Katarina",
+          "incorrect_answers": [
+              "Shaco",
+              "Akali",
+              "Zed"
+          ]
+      },
+      {
+          "category": "Science: Gadgets",
+          "type": "multiple",
+          "difficulty": "hard",
+          "question": "Which round does a WW2 M1 Garand fire?",
+          "correct_answer": ".30-06",
+          "incorrect_answers": [
+              ".308",
+              "7.62",
+              "7.62x51mm"
+          ]
+      }
+  ]
+};
+
+export default questions;
