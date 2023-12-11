fb4c42925f00b04ffdcc243d5d7672fcf2c9fec9
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-12-11 14:35:51 -0300

    terminando login page do trivia

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { savePlayerInfo } from '../redux/actions';
-import './Login.css';
+import triviaLogo from '../svgs/login/triviaLogo.svg';
+import trybe from '../svgs/login/trybeLogo.svg';
+import engine from '../svgs/login/miniEngine.svg';
 
 class Login extends React.Component {
   state = {
@@ -30,60 +32,73 @@ class Login extends React.Component {
     const { name, email } = this.state;
     const { history: { push }, dispatch } = this.props;
     return (
-      <div className="container">
-        <div className="labels">
-          <label htmlFor="name">
-            Nome
-            <input
-              id="name"
-              data-testid="input-player-name"
-              type="text"
-              value={ name }
-              name="name"
-              onChange={ this.handleChange }
-              placeholder="Digite seu nome"
-            />
-          </label>
-          <label htmlFor="email">
-            Email
-            <input
-              id="email"
-              data-testid="input-gravatar-email"
-              type="email"
-              value={ email }
-              name="email"
-              onChange={ this.handleChange }
-              placeholder="Digite seu Email"
-            />
-          </label>
-        </div>
-        <div className="buttoons">
+      <div className="flex items-center flex-col justify-center">
+        <header className="mb-[12px] mt-[40px]">
+
+          <img src={ triviaLogo } alt="" />
+        </header>
+
+        <main
+          className="w-[614px] h-[340px] rounded-[10px] flex flex-col items-center
+        justify-center bg-white shadow-[0px 4px 4px rgba(0, 0, 0, 0.25)] gap-[20px]"
+        >
+          <input
+            id="name"
+            data-testid="input-player-name"
+            type="text"
+            value={ name }
+            name="name"
+            onChange={ this.handleChange }
+            placeholder="Digite seu nome"
+            className="border-[1px] border-[#E1E5EB]
+            w-[519px] h-[45px] pt-[12px] pb-[12px] pl-[16px] pr-[16px]"
+          />
+          <input
+            id="email"
+            data-testid="input-gravatar-email"
+            type="email"
+            value={ email }
+            className="border-[1px] border-[#E1E5EB]
+            w-[519px] h-[45px] pt-[12px] pb-[12px] pl-[16px] pr-[16px]"
+            name="email"
+            onChange={ this.handleChange }
+            placeholder="Digite seu Email"
+          />
 
           <button
-            className="button"
+            className="w-[519px] h-[45px] bg-[#2FC18C] flex items-center justify-center
+            gap-[10px] rounded-[5px] text-white font-bold text-[18px] cursor-pointer
+            hover:bg-[#55f8bc] transition-colors duration-1000 hover:text-black"
             data-testid="btn-play"
             disabled={ !(name.length > 0 && email.length > 0) }
             onClick={ async () => {
-            // Temos que fazer um tratamendo para caso retorne o response_code 0 ou 3
+              // Temos que fazer um tratamendo para caso retorne o response_code 0 ou 3
               const token = await this.getToken();
               dispatch(savePlayerInfo({ email, name }));
               localStorage.setItem('token', token.token);
               push('/game');
             } }
           >
-            Play
+            JOGAR
           </button>
           <Link to="/settings">
             <button
-              className="buttonConf"
+              className="w-[519px] h-[45px] bg-[#35906F] flex items-center justify-center
+              gap-[10px] rounded-[5px] text-white font-bold text-[18px]
+              hover:bg-[#173d2f] transition-colors duration-1000 hover:text-black"
               data-testid="btn-settings"
             >
+              <img src={ engine } alt="" />
               Configurações
 
             </button>
           </Link>
-        </div>
+        </main>
+        <footer className="mt-[40px]">
+          <img src={ trybe } alt="" />
+        </footer>
       </div>
+
     );
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,6 @@ module.exports = {
   theme: {
     extend: {},
   },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   plugins: [],
 };
